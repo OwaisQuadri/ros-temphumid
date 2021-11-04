d67c8f68ff50fdb2646aa1ec78cd5981c8d1caae
Owais <owais.quadri@ontariotechu.net>
2021-11-04 03:42:59 -0400

    remove custom middleware

--- IoTThermometer/settings.py
+++ IoTThermometer/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    'app.middle.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
